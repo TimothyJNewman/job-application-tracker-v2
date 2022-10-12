bd256307006b241ff75b17368afdb1ce75ee1046
TimothyJNewman <84350177+TimothyJNewman@users.noreply.github.com>
2022-10-13 00:33:25 +0100

    fix: summary page scroll issue

--- src/renderer/pages/ApplicationSummaryPage/ApplicationSummaryPage.jsx
+++ src/renderer/pages/ApplicationSummaryPage/ApplicationSummaryPage.jsx
@@ -324,8 +324,8 @@ const ApplicationSummaryPage = () => {
         ''
       )}
       <div className='flex flex-col'>
-        <div className='overflow-x-auto sm:-mx-6 lg:-mx-8'>
-          <div className='inline-block min-w-full py-2 sm:px-6 lg:px-8'>
+        <div className='overflow-x-auto'>
+          <div className='inline-block min-w-full py-2'>
             <div className='overflow-hidden'>
               <table className='min-w-full'>
                 <thead className='border-b bg-white'>
